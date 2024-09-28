2227c82125f2778b07eb426ed04ae796910a21eb
CyrusSmith2099 <148400158+CyrusSmith2099@users.noreply.github.com>
2024-09-27 19:25:18 -0930

    Added player and walking mechanic

--- src/player_movement.gd
+++ src/player_movement.gd
@@ -1,11 +1,56 @@
-extends Node
+extends CharacterBody2D
 
+@export var speed: float = 5000.0
+@export var stop_distance: float = 5.0
+@export var move_threshold: float = 10.0
+@export var tolerance: float = 1.0  # Tolerance for small position changes
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var animation = $AnimationPlayer
+@onready var sprite = $Sprite2D
+@onready var anchor = $Anchor
 
+var target_position = Vector2()
+var is_moving = false
+var last_position = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _ready():
+	target_position = anchor.global_position
+	last_position = anchor.global_position
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var new_target_position = get_global_mouse_position()
+		if new_target_position.distance_to(anchor.global_position) > move_threshold:
+			target_position = new_target_position
+			is_moving = true
+
+func _physics_process(delta):
+	if not is_moving:
+		handle_idle_state()
+		return
+	
+	var direction = (target_position - anchor.global_position).normalized()
+	print(direction)
+	velocity = direction * speed * delta
+	
+	var distance_to_target = anchor.global_position.distance_to(target_position)
+	if distance_to_target > stop_distance:
+		move_and_slide()
+		update_sprite_direction(direction)
+		animation.play("Walk")
+
+		# Check if the player is stuck
+		if last_position.distance_to(anchor.global_position) < tolerance:
+			is_moving = false
+			velocity = Vector2.ZERO
+		last_position = anchor.global_position # Update last position
+	else:
+		is_moving = false
+		velocity = Vector2.ZERO
+
+func update_sprite_direction(direction):
+	sprite.flip_h = direction.x < 0
+
+func handle_idle_state():
+	velocity = Vector2.ZERO
+	animation.play("None")
