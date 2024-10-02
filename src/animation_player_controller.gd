9a1137088f90d49eccea7ab9761a405b23561211
CyrusSmith2099 <148400158+CyrusSmith2099@users.noreply.github.com>
2024-10-01 18:14:19 -0930

    Improving playback feature

--- src/animation_player_controller.gd
+++ src/animation_player_controller.gd
@@ -4,24 +4,39 @@ extends Node2D
 @export var animation_name: String = ""
 
 @onready var menu: Control = $Menu
+@onready var playback_button: Button = menu.get_node("PlaybackButton")
 @onready var progress_bar: ProgressBar = menu.get_node("ProgressBar")
 var is_dragging: bool = false
+var last_time = 0
+var is_paused: bool = false
+var tolerance: float = 0.01  # Tolerance for animation end comparison
+
+# Signals used in other scripts
+@warning_ignore("unused_signal") 
+signal animation_forward(is_forward: bool)
 
 func _ready():
 	# Make animation_name the current animation
 	animation.play(animation_name)
-	animation.stop(false) # Pause the animation but don't reset state
+	last_time = animation.current_animation_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# Ensure signal is connected only once
+	playback_button.connect("paused", Callable(self, "_on_paused"))
+
 func _process(delta):
 	if not is_dragging and animation.is_playing():
 		progress_bar.value = animation.current_animation_position / animation.get_current_animation_length() * 100
-	
+
 	# Seek with arrow keys (optional)
 	if Input.is_action_pressed("ui_left"):
 		animation.seek(animation.current_animation_position - delta, true)
 	elif Input.is_action_pressed("ui_right"):
 		animation.seek(animation.current_animation_position + delta, true)
+
+	# Check if animation reaches the end using tolerance for floating point precision
+	if animation.current_animation_position >= animation.get_current_animation_length() - tolerance:
+		playback_button.is_paused = true
+		playback_button.emit_signal("paused", true)
 
 func _input(event):
 	# Check if the mouse is pressed and dragging the progress bar
@@ -36,9 +51,26 @@ func _input(event):
 		progress_bar.value = progress * 100
 		
 		# Update the animation's position based on progress
+		if not animation.is_playing():
+			animation.play(animation_name)
 		var new_time = progress * animation.get_current_animation_length()
 		animation.seek(new_time, true)
+
+		# Detect if animation is being played backwards or forwards
+		emit_signal("animation_forward", new_time > last_time)
+		last_time = new_time
 	
-	# Detect when the mouse is released to stop dragging
+	# Detect when the mouse is released, even outside the bounds of the progress bar
 	if event is InputEventMouseButton and not event.pressed:
 		is_dragging = false
+
+func _on_paused(state: bool):
+	is_paused = state
+	if is_paused:
+		animation.set_speed_scale(0)
+	else:
+		# If the animation has reached the end, reset it before playing again
+		if animation.current_animation_position >= animation.get_current_animation_length() - tolerance:
+			animation.play(animation_name)
+		animation.set_speed_scale(1)
+	print("Paused: ", is_paused)
--- src/character_animation_controller.gd
+++ src/character_animation_controller.gd
@@ -1,10 +1,35 @@
 extends CharacterBody2D
-@export var player: AnimationPlayer
+
 @export var animation_name = ""
 
+var is_paused: bool = false
+var is_forward: bool = false
+
+@onready var main_scene: Node = get_tree().get_root().get_node("MainScene")
+@onready var playback_button: Button = main_scene.get_node("Menu").get_node("PlaybackButton")
+@onready var player: AnimationPlayer = self.get_node("AnimationPlayer")
+
 func _ready():
-	pass
+	if playback_button:
+		playback_button.connect("paused", Callable(self, "_on_paused"))
+	if main_scene:
+		main_scene.connect("animation_forward", Callable(self, "_on_change_animation_direction"))
 
 func _physics_process(_delta):
 	if player and animation_name and animation_name != player.current_animation and animation_name != "":
-		player.play(animation_name)
+		if is_forward:
+			player.play(animation_name)
+		else:
+			player.play_backwards(animation_name)
+
+func _on_paused(new_paused_state: bool):
+	self.is_paused = new_paused_state
+	if is_paused:
+		player.set_speed_scale(0)
+	else:
+		player.set_speed_scale(1)
+	#print("Paused: ", is_paused)
+
+func _on_change_animation_direction(state: bool):
+	self.is_forward = state
+	#print("Forward: ", is_forward)
