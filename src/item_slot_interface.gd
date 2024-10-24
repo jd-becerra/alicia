166cdd32219e5d3dc5be0b67d02eb91908053df4
CyrusSmith2099 <148400158+CyrusSmith2099@users.noreply.github.com>
2024-10-23 22:53:15 -0600

    Refactor item_slot_interface.gd to improve item selection and mouse interaction

--- src/item_slot_interface.gd
+++ src/item_slot_interface.gd
@@ -4,32 +4,36 @@ extends PanelContainer
 @onready var margin_container = $MarginContainer
 @onready var texture_rect = margin_container.get_node("TextureRect")
 
+var item_data: Item
+
+@warning_ignore("unused_signal")
 signal item_slot_selected(item: Item, index: int)
 
 func _ready() -> void:
-    gui_input.connect(_on_gui_input)
+	gui_input.connect(_on_gui_input)
 
-func set_data(item: Item):
-    texture_rect.texture = item.texture
-    tooltip_text = "%s" % item.description
+func set_data(item: Item) -> void:
+	item_data = item
+	texture_rect.texture = item.texture
+	tooltip_text = "%s" % item.description
 
 func _process(_delta: float) -> void:
-    # Make texture bigger if mouse is over the item by making the margin smaller
-    if is_mouse_over():
-        resize_margin(0)
-    else:
-        resize_margin(8)
+	# Make texture bigger if mouse is over the item by making the margin smaller
+	if is_mouse_over():
+		resize_margin(0)
+	else:
+		resize_margin(8)
 
 func is_mouse_over() -> bool:
-    return texture_rect.get_global_rect().has_point(get_viewport().get_mouse_position())
+	return texture_rect.get_global_rect().has_point(get_viewport().get_mouse_position())
 
 func resize_margin(margin: int) -> void:
-    margin_container.add_theme_constant_override("margin_left", margin)
-    margin_container.add_theme_constant_override("margin_right", margin)
-    margin_container.add_theme_constant_override("margin_top", margin)
-    margin_container.add_theme_constant_override("margin_bottom", margin)
+	margin_container.add_theme_constant_override("margin_left", margin)
+	margin_container.add_theme_constant_override("margin_right", margin)
+	margin_container.add_theme_constant_override("margin_top", margin)
+	margin_container.add_theme_constant_override("margin_bottom", margin)
 
 func _on_gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            emit_signal("item_slot_selected", self, get_index())
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			emit_signal("item_slot_selected", item_data, get_index())
